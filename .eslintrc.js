ef9ccfeba68d9c019aee283ed6de50519e26c37b
camcamca <23409643+camcamca@users.noreply.github.com>
2020-12-20 10:20:01 -0700

    Setting up repository for TDD.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,21 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
+  plugins: ['jest'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
   rules: {
     'import/extensions': ['warn', 'always'],
+    'no-use-before-define': [
+      'error',
+      {
+        functions: false,
+        variables: true,
+      },
+    ],
+    'no-console': 'off',
   },
 };
